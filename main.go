c41b3ecef6193ae64eac319f71ae4c6ed4eb79c3
MatheusKael <matheuskael.silva@gmail.com>
2023-05-17 15:45:09 -0400

    feat(Cache): cache is now been set a moment before the window is closed

--- main.go
+++ main.go
@@ -18,6 +18,11 @@ import (
 
 const tempDir = "./tmp/body.json"
 
+type Data struct {
+	URL  string `json:"Url"`
+	Body interface{}
+}
+
 func main() {
 
 	app := app.New()
@@ -35,33 +40,26 @@ func main() {
 	body := widget.NewMultiLineEntry()
 
 	responseUi := widget.NewLabel("Response")
-	bodyData, err := os.OpenFile(tempDir, os.O_RDWR|os.O_CREATE, 0666)
+	cacheData, err := os.OpenFile(tempDir, os.O_RDWR|os.O_CREATE, 0666)
 
 	stats, err := os.Stat(tempDir)
 
-	defer bodyData.Close()
+	defer cacheData.Close()
 
 	data := make([]byte, stats.Size())
 
-	_, err = bodyData.Read(data)
+	_, err = cacheData.Read(data)
+	var jsonTmpData struct {
+		Data []Data `json:"Data"`
+	}
+
+	err = json.Unmarshal(data, &jsonTmpData)
 	if err != nil {
 		panic(err)
 	}
 
-	body.SetText(string(data))
-
-	body.OnChanged = func(text string) {
-
-		bodyData.Truncate(1)
-		bodyData.Seek(1, 0)
-
-		_, err = bodyData.Write([]byte(text))
-
-		if err != nil {
-			panic(err)
-		}
-
-	}
+	bodyData, err := json.Marshal(jsonTmpData.Data[0].Body)
+	body.SetText(string(bodyData))
 	httpMethod := &widget.Select{Options: []string{"GET", "POST", "PUT"}}
 
 	button := widget.NewButton("Enter", func() {
@@ -96,11 +94,36 @@ func main() {
 
 	header := container.NewGridWithColumns(3, httpMethod, url, container.NewHBox(button))
 
-	border := container.NewBorder(header, nil, nil, nil, container.NewHSplit(body, container.NewScroll(responseUi)))
+	tab := container.NewTabItem("Body", body)
+
+	appTabs := container.NewAppTabs(tab)
+
+	border := container.NewBorder(header, nil, nil, nil, container.NewHSplit(appTabs, container.NewScroll(responseUi)))
 
 	window.SetContent(border)
 
 	window.Resize(fyne.NewSize(1000, 600))
+
+	window.SetOnClosed(func() {
+
+		cacheData, err := os.OpenFile(tempDir, os.O_RDWR|os.O_CREATE, 0666)
+
+		cacheData.Truncate(0)
+		cacheData.Seek(0, 0)
+
+		defer cacheData.Close()
+
+		conver, err := json.Marshal(body.Text)
+
+		jsonTmpData.Data[0].Body = conver
+
+		_, err = cacheData.Write([]byte(string(conver)))
+
+		if err != nil {
+			panic(err)
+		}
+
+	})
 
 	window.ShowAndRun()
 
